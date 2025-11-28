2e6f61ac91bf9e03c9d1245db043c09e59d6cd20
viallymboma <viallymboma@gmail.com>
2025-11-28 03:59:51 +0100

    fixed

--- src/lib/keycloak.ts
+++ src/lib/keycloak.ts
@@ -1,4 +1,6 @@
 // lib/keycloak.ts
+import Keycloak from "keycloak-js";
+
 export const KEYCLOAK_BASE = process.env.KEYCLOAK_BASE_URL!;
 export const REALM = process.env.KEYCLOAK_REALM!;
 export const CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID!;
@@ -7,10 +9,9 @@ export const ISSUER = process.env.KEYCLOAK_ISSUER; // `${KEYCLOAK_BASE}/realms/$
 export const TOKEN_ENDPOINT = `${ISSUER}/protocol/openid-connect/token`;
 export const AUTH_ENDPOINT = `${ISSUER}/protocol/openid-connect/auth`;
 export const LOGOUT_ENDPOINT = `${ISSUER}/protocol/openid-connect/logout`;
-export const JWKS_URI = `${ISSUER}/protocol/openid-connect/certs`;
+export const JWKS_URI = `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/certs`;
 export const REDIRECT_URI = `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`;
 
-import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
   url: "https://www.dev.lesastiqueuses.fr/sso",
